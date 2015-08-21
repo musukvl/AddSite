31ff7590d0478ada2a12e2a73c207c08770a4192
Alexey Ryazhskikh <musukvl@gmail.com>
2015-08-21 14:02:04 +0300

    AddSite

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using NConsoler;
+
+namespace Musuk.AddSite
+{
+    class Program
+    {
+        public static void Main(params string[] args)
+        {
+            Consolery.Run(typeof(Program), args);
+        }
+
+        [Action]
+        public static void Method(
+            [Required(Description = "Host name for site binding. Site will have the same name too.")] 
+            string hostName,
+            [Optional(null, "path", Description = "Site home directory path. Current directory by default")] 
+            string physicalPath,
+            [Optional("127.0.0.1", Description = "Site binding IP. [127.0.0.1] by default")] 
+            string ip,
+            [Optional(80, Description = "Site binding port. [80] by default.")]
+            int port,
+            [Optional("Classic .NET AppPool", "pool", Description = "Application pool for site. [Classic .NET AppPool] by default")]
+            string appPool
+        )
+        {
+            try
+            {
+                ProcessBuildSite(ip, physicalPath, hostName, appPool, port);
+                Console.WriteLine("Done.");
+            }
+            catch(FileNotFoundException e)
+            {
+                WriteError("{0} file {1}", e.Message, e.FileName);
+            }
+            catch (Exception e)
+            {
+                WriteError(e.Message);
+            }
+        }
+
+        private static void WriteError(string message, params string[] param)
+        {
+            Console.WriteLine("Site creating failed: {0}", string.Format(message, param));
+        }
+
+        private static void ProcessBuildSite(string ip, string physicalPath, string hostName, string appPool, int port)
+        {
+            using (var siteBuilder = new SiteBuilder())
+            {
+                if (!siteBuilder.IsValidIP(ip))
+                {
+                    Console.WriteLine("IP address [{0}] has invalid format.", ip);
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(physicalPath))
+                    physicalPath = Directory.GetCurrentDirectory();
+                if (siteBuilder.SiteExists(hostName))
+                {
+                    Console.WriteLine("Site with name {0} already exists.", hostName);
+                    return;
+                }
+                if (!siteBuilder.AppPoolExists(appPool))
+                {
+                    Console.WriteLine("Application pool {0} does not exists.", appPool);
+                    Console.WriteLine("Available application pool names are:");
+                    foreach (var pool in siteBuilder.ServerManager.ApplicationPools)
+                    {
+                        Console.WriteLine(pool.Name);
+                    }
+                    return;
+                }
+
+                siteBuilder.CreateSite(ip, port, hostName, physicalPath, appPool);
+                Console.WriteLine("Site was created.");
+
+                if (!siteBuilder.HostFileRecordExists(ip, hostName))
+                {
+                    siteBuilder.AppendHostsFile(ip, hostName);
+                    Console.WriteLine("Hosts file was recored appended.");
+                }
+                else
+                    Console.WriteLine("Hosts file recored for this ip/host already exists.");
+            }
+        }
+    }
+}
---
+++ SiteBuilder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Web.Administration;
+
+namespace Musuk.AddSite
+{
+    public class SiteBuilder : IDisposable
+    {
+        public ServerManager ServerManager
+        {
+            get; 
+            protected set;
+        }
+
+        public SiteBuilder()
+        {
+            this.ServerManager = new ServerManager();
+        }
+        
+        public bool SiteExists(string siteName)
+        {
+            return ServerManager.Sites.Any(s => s.Name == siteName);
+        } 
+
+        public bool AppPoolExists(string appPoolName)
+        {
+            return ServerManager.ApplicationPools.Any(p => p.Name == appPoolName);
+        }
+
+        public string BuildBindingInfo(string ip, string port, string hostName)
+        {
+            return string.Format("{0}:{1}:{2}", ip, port, hostName);
+        }
+
+        public void CreateSite(string siteName, string bindingInfo, string physicalPath, string appPool)
+        {
+            var site = ServerManager.Sites.Add(siteName, "http", bindingInfo, physicalPath);            
+            site.Applications[0].ApplicationPoolName = appPool;            
+            site.ServerAutoStart = true;
+            ServerManager.CommitChanges();
+        }
+
+        public void CreateSite(string ip, int port, string hostName, string physicalPath, string appPool)
+        {
+            var bindingInfo = BuildBindingInfo(ip, port.ToString(), hostName);
+            CreateSite(hostName, bindingInfo, physicalPath, appPool);
+        }
+
+        private string GetHostsFilePath()
+        {
+            return System.Environment.ExpandEnvironmentVariables(@"%WinDir%/System32/drivers/etc/hosts");
+        }
+
+        public bool HostFileRecordExists(string ip, string hostname)
+        {
+            string hostsFile;
+            using (var stream = new StreamReader(File.OpenRead(GetHostsFilePath())))
+            {
+                hostsFile = stream.ReadToEnd();
+            }
+            var matchRecordRegexPattern = string.Format(@"{0}\s+{1}", Regex.Escape(ip), Regex.Escape(hostname));
+            return Regex.Match(hostsFile, matchRecordRegexPattern).Success;
+        }
+
+        public void AppendHostsFile(string ip, string hostname)
+        {
+            var hostsFilePath = GetHostsFilePath();
+            if (!File.Exists(hostsFilePath))
+                throw new FileNotFoundException("Hosts file not found", hostsFilePath);
+            bool addNewLineBeforeRecord = (!File.ReadAllText(hostsFilePath).EndsWith(Environment.NewLine));
+            
+            using (var appender = File.AppendText(hostsFilePath))
+            {
+                if (addNewLineBeforeRecord)
+                    appender.WriteLine();
+                appender.WriteLine("{0} \t{1}", ip, hostname);
+            }
+        }
+
+        
+
+        public bool IsValidIP(string addr)
+        {
+            const string pattern = @"^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$";
+            Regex check = new Regex(pattern, RegexOptions.Compiled);
+            return check.IsMatch(addr, 0);
+        }
+
+        public void Dispose()
+        {
+            ServerManager.Dispose();
+        }
+    }
+}
